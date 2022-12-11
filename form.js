b945f18b2d27b2852a3b158f6a813c7015e2e5da
Jabed Hasan <60073739+JabedHossainJabuu@users.noreply.github.com>
2022-12-12 00:49:33 +0600

    second commit

--- form.js
+++ form.js
@@ -14,7 +14,15 @@ form.addEventListener('submit', (e) =>{
     if (name.value=='' || name.value==null) {
         messeges.push('Name is required')
     }
-
+    if(password.value.length <=6){
+        messeges.push("password must be longer than 6 characters")
+    }
+    if(password.value.length >=20){
+        messeges.push("password must be less than 20 characters")
+    }
+    if(password.value=='password'){
+        messeges.push("Password can't be password")
+    }
     if(messeges.length > 0){
         e.preventDefault()
         errorElement.innerText = messeges.join(', ')
